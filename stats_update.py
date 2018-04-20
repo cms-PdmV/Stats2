8c35459208ca2846f69770af6903232de07c3afc
Justinas Rumševičius <justinas.rumsevicius@cern.ch>
2018-04-20 13:52:21 +0200

    Merge update and updater into single file, do not show -1 total events

--- stats_update.py
+++ stats_update.py
@@ -1,7 +1,10 @@
 import logging
+import time
+import argparse
+import json
+from utils import setup_console_logging
 from database import Database
 from utils import make_request_with_grid_cert, pick_attributes
-import time
 
 
 class StatsUpdate():
@@ -261,3 +264,34 @@ class StatsUpdate():
 
         self.logger.info('Found %d requests for changed datasets' % (len(requests)))
         return requests
+
+
+def main():
+    setup_console_logging()
+    logger = logging.getLogger('logger')
+    parser = argparse.ArgumentParser(description='Stats2 update')
+    parser.add_argument('--action',
+                        choices=['update', 'see', 'drop'],
+                        required=True,
+                        help='Action to be performed.')
+    parser.add_argument('--name',
+                        required=False,
+                        help='Request to be updated.')
+    args = vars(parser.parse_args())
+    logger.info('Arguments %s' % (str(args)))
+
+    action = args.get('action', None)
+    name = args.get('name', None)
+
+    if action == 'update':
+        stats_update = StatsUpdate()
+        stats_update.perform_update(name)
+    elif action == 'see':
+        request = Database().get_request(name)
+        print(json.dumps(request, indent=4))
+    elif action == 'drop':
+        Database().clear_database()
+
+
+if __name__ == '__main__':
+    main()
--- stats_updater.py
+++ stats_updater.py
@@ -1,37 +1,4 @@
-import argparse
-import logging
-from stats_update import StatsUpdate
-from database import Database
-import json
-from utils import setup_console_logging
 
 
-def main():
-    setup_console_logging()
-    logger = logging.getLogger('logger')
-    parser = argparse.ArgumentParser(description='Stats2 update')
-    parser.add_argument('--action',
-                        choices=['update', 'see', 'drop'],
-                        required=True,
-                        help='Action to be performed.')
-    parser.add_argument('--name',
-                        required=False,
-                        help='Request to be updated.')
-    args = vars(parser.parse_args())
-    logger.info('Arguments %s' % (str(args)))
-
-    action = args.get('action', None)
-    name = args.get('name', None)
-
-    if action == 'update':
-        stats_update = StatsUpdate()
-        stats_update.perform_update(name)
-    elif action == 'see':
-        request = Database().get_request(name)
-        print(json.dumps(request, indent=4))
-    elif action == 'drop':
-        Database().clear_database()
 
 
-if __name__ == '__main__':
-    main()
