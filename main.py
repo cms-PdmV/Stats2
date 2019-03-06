9c6a5d9e7e148bbd2a1615cc2dc56fb6c5dc1f98
Justinas Rumševičius <justinas.rumsevicius@cern.ch>
2019-03-06 11:28:39 +0100

    Adjust for reverse proxy, add api methods

--- main.py
+++ main.py
@@ -32,50 +32,35 @@ def check_with_old_stats(workflows):
             req['TotalEventsEqual'] = 'not_found'
 
 
+@app.route('/get_json/<string:workflow_name>')
+@app.route('/api/get_json/<string:workflow_name>')
+def html_view_json(workflow_name):
+    database = Database()
+    workflow = database.get_workflow(workflow_name)
+    if workflow is None:
+        response = make_response("{}", 404)
+    else:
+        response = make_response(json.dumps(workflow, indent=2, sort_keys=True), 200)
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+# HTML responses
 @app.route('/')
 @app.route('/<int:page>')
-def index(page=0):
+def html_get(page=0):
     database = Database()
-    prepid = request.args.get('prepid')
-    dataset = request.args.get('dataset')
-    campaign = request.args.get('campaign')
-    workflow_type = request.args.get('type')
-    workflow_name = request.args.get('workflow_name')
-    processing_string = request.args.get('processing_string')
-    request_name = request.args.get('request')
+    workflows = get_page(page)
+    pages = [page, page > 0, database.PAGE_SIZE == len(workflows)]
+    workflows = list(filter(lambda req: '_design' not in req['_id'], workflows))
     check = request.args.get('check')
-    if page < 0:
-        page = 0
-
-    if workflow_name is not None:
-        req = database.get_workflow(workflow_name)
-        if req is not None:
-            workflows = [req]
-        else:
-            workflows = []
-
-    else:
-        if prepid is not None:
-            workflows = database.get_workflows_with_prepid(prepid, page=page, include_docs=True)
-        elif dataset is not None:
-            workflows = database.get_workflows_with_dataset(dataset, page=page, include_docs=True)
-        elif campaign is not None:
-            workflows = database.get_workflows_with_campaign(campaign, page=page, include_docs=True)
-        elif workflow_type is not None:
-            workflows = database.get_workflows_with_type(workflow_type, page=page, include_docs=True)
-        elif processing_string is not None:
-            workflows = database.get_workflows_with_processing_string(processing_string, page=page, include_docs=True)
-        elif request_name is not None:
-            workflows = database.get_workflows_with_request(request_name, page=page, include_docs=True)
-        else:
-            workflows = database.get_workflows(page=page, include_docs=True)
-
     if check is not None:
         check_with_old_stats(workflows)
 
-    pages = [page, page > 0, database.PAGE_SIZE == len(workflows)]
-    workflows = list(filter(lambda req: '_design' not in req['_id'], workflows))
     for req in workflows:
+        if '_design' in req['_id']:
+            continue
+
         req['DonePercent'] = '0.00'
         req['OpenPercent'] = '0.00'
         req['LastDatasetType'] = 'NONE'
@@ -84,7 +69,11 @@ def index(page=0):
         req['LastUpdate'] = time.strftime('%Y&#8209;%m&#8209;%d&nbsp;%H:%M:%S', time.localtime(req['LastUpdate']))
 
         if len(req.get('RequestTransition', [])) > 0:
-            req['LastStatus'] = req['RequestTransition'][-1].get('Status', '-')
+            last_transition = req['RequestTransition'][-1]
+            if 'Status' in last_transition and 'UpdateTime' in last_transition:
+                req['LastStatus'] = '%s (%s)' % (last_transition['Status'], time.strftime('%Y&#8209;%m&#8209;%d&nbsp;%H:%M:%S', time.localtime(last_transition['UpdateTime'])))
+            else:
+                req['LastStatus'] = last_transition.get('Status', '-')
         else:
             req['LastStatus'] = '-'
 
@@ -119,65 +108,90 @@ def index(page=0):
                            query=request.query_string.decode('utf-8'))
 
 
-@app.route('/update/<string:workflow_name>')	
-def update(workflow_name):	
-    StatsUpdate().perform_update(workflow_name=workflow_name)	
-    return redirect("/0?workflow_name=" + workflow_name, code=302)
+@app.route('/update/<string:workflow_name>')
+def html_update(workflow_name): 
+    StatsUpdate().perform_update(workflow_name=workflow_name)
+    return redirect("/?workflow_name=" + workflow_name, code=302)
 
 
-@app.route('/get/<string:workflow_name>')
-def get_one(workflow_name):
-    database = Database()
-    workflow = database.get_workflow(workflow_name)
-    if workflow is None:
-        response = make_response("{}", 404)
-    else:
-        response = make_response(json.dumps(workflow), 200)
-
+# JSON responses
+@app.route('/api/get')
+@app.route('/api/get/<int:page>')
+def api_get(page=0):
+    workflows = get_page(page)
+    workflows = list(filter(lambda req: '_design' not in req['_id'], workflows))
+    response = make_response(json.dumps(workflows,
+                                        indent=4,
+                                        sort_keys=True),
+                             200)
     response.headers['Content-Type'] = 'application/json'
     return response
 
 
-@app.route('/view_json/<string:workflow_name>')
-def get_nice_json(workflow_name):
-    database = Database()
-    workflow = database.get_workflow(workflow_name)
-    if workflow is None:
-        response = make_response("{}", 404)
-    else:
-        response = make_response(json.dumps(workflow, indent=4, sort_keys=True), 200)
+@app.route('/api/update/<string:workflow_name>')
+def api_update(workflow_name):
+    StatsUpdate().perform_update(workflow_name=workflow_name)
+    return redirect("/api/get?workflow_name=" + workflow_name, code=302)
 
-    response.headers['Content-Type'] = 'application/json'
-    return response
+
+# Actual get method
+def get_page(page=0):
+    database = Database()
+    prepid = request.args.get('prepid')
+    dataset = request.args.get('dataset')
+    campaign = request.args.get('campaign')
+    workflow_type = request.args.get('type')
+    workflow_name = request.args.get('workflow_name')
+    processing_string = request.args.get('processing_string')
+    request_name = request.args.get('request')
+    if page < 0:
+        page = 0
+
+    if workflow_name is not None:
+        req = database.get_workflow(workflow_name)
+        if req is not None:
+            workflows = [req]
+        else:
+            workflows = []
+
+    else:
+        if prepid is not None:
+            workflows = database.get_workflows_with_prepid(prepid, page=page, include_docs=True)
+        elif dataset is not None:
+            workflows = database.get_workflows_with_dataset(dataset, page=page, include_docs=True)
+        elif campaign is not None:
+            workflows = database.get_workflows_with_campaign(campaign, page=page, include_docs=True)
+        elif workflow_type is not None:
+            workflows = database.get_workflows_with_type(workflow_type, page=page, include_docs=True)
+        elif processing_string is not None:
+            workflows = database.get_workflows_with_processing_string(processing_string, page=page, include_docs=True)
+        elif request_name is not None:
+            workflows = database.get_workflows_with_request(request_name, page=page, include_docs=True)
+        else:
+            workflows = database.get_workflows(page=page, include_docs=True)
+
+    return workflows
 
 
 def run_flask():
     setup_file_logging()
-    parser = argparse.ArgumentParser(description='Stats2 update')
-    parser.add_argument('--ssl_cert',
-                        help='Path to a certificate for HTTPS')
-    parser.add_argument('--ssl_key',
-                        help='Path to a certificate for HTTPS')
+    parser = argparse.ArgumentParser(description='Stats2')
     parser.add_argument('--port',
-                        help='Port on which website will be listening. Default 80 if no ssl_cert and not ssl_key key is specified, 443 otherwise')
-
+                        help='Port, default is 8001')
+    parser.add_argument('--host',
+                        help='Host IP, default is 127.0.0.1')
     args = vars(parser.parse_args())
-    ssl_cert = args.get('ssl_cert', None)
-    ssl_key = args.get('ssl_key', None)
     port = args.get('port', None)
-    if ssl_cert and ssl_key:
-        ssl_context = (ssl_cert, ssl_key)
-        if not port:
-            port = 443
-    else:
-        ssl_context = None
-        if not port:
-            port = 80
+    host = args.get('host', None)
+    if not port:
+        port = 8001
 
-    app.run(host='0.0.0.0',
+    if not host:
+        host = '127.0.0.1'
+
+    app.run(host=host,
             port=port,
-            threaded=True,
-            ssl_context=ssl_context)
+            threaded=True)
 
 
 if __name__ == '__main__':
